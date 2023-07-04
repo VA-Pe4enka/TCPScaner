529d72168f498c111454d169b3eb305b0f0c4c53
VA-Pe4enka <va.pe4enka@gmail.com>
2023-07-04 13:02:18 +0300

    Add files

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+func TCPscanner(ports chan int, result chan int) {
+
+	for p := range ports {
+		address := fmt.Sprintf("localhost:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err == nil {
+			result <- p
+			fmt.Println(result)
+			defer conn.Close()
+			break
+		} else {
+			continue
+		}
+
+	}
+	//fmt.Println("End of TCP scanner")
+	//fmt.Println("End")
+}
+
+func TLSscanner(result chan int, openports map[int]string) {
+
+	conf := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	for r := range result {
+		fmt.Println(r)
+		address := fmt.Sprintf("localhost:%d", r)
+		connTLS, err := tls.DialWithDialer(&net.Dialer{Timeout: 3 * time.Second}, "tcp", address, conf)
+		if err != nil {
+			fmt.Println("1:", err)
+			openports[r] = "no TLS"
+			fmt.Println(openports)
+			break
+		} else {
+			fmt.Println("print cert:", connTLS.ConnectionState().PeerCertificates)
+			openports[r] = "TLS"
+			fmt.Println(openports)
+		}
+		connTLS.Close()
+
+	}
+	fmt.Println("End of TLS scanner")
+}
+
+func main() {
+	var wg sync.WaitGroup
+	openports := make(map[int]string)
+	ports := make(chan int, 100)
+	result := make(chan int)
+
+	go func() {
+		fmt.Println("Taking ports")
+		for i := 0; i <= 1024; i++ {
+			ports <- i
+		}
+		fmt.Println("Close channel ports")
+		close(ports)
+	}()
+
+	for i := 0; i < cap(ports); i++ {
+		wg.Add(1)
+		go TCPscanner(ports, result)
+		go TLSscanner(result, openports)
+		wg.Done()
+
+	}
+
+	for i := 0; i < cap(ports); i++ {
+		port := <-result
+		if port == 0 {
+			return
+		}
+	}
+
+	wg.Wait()
+
+	//defer close(result)
+	//defer close(ports)
+
+	fmt.Println(openports)
+}
